3e802084323b0aa499f3769295d29fa6f4110e55
Augusto Leal <augustocmleal@gmail.com>
2023-12-13 18:21:14 -0300

    Projeto completo inclusive com libs e arquivos de configuração

---
+++ Assets/Scripts/Player.cs
@@ -0,0 +1,59 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+public class Player : MonoBehaviour
+{
+
+    
+
+    public float moveSpeed = 5.0f; // Adjust this to control the character's speed.
+    private Vector3 targetPosition;
+    private bool isMoving = false;
+
+    Animator myAnimator;
+    // Start is called before the first frame update
+    void Start()
+    {
+        myAnimator = gameObject.GetComponent<Animator>();
+    }
+
+    void Update()
+    {
+        if (Input.GetMouseButtonDown(0))
+        {
+            // Raycast from the mouse click position to the ground or terrain.
+            Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+            RaycastHit hit;
+
+            if (Physics.Raycast(ray, out hit))
+            {
+                // Get the target position from the raycast hit.
+                targetPosition = hit.point;
+                isMoving = true;
+            }
+        }
+          // Rotate the character to face the target.
+              if (isMoving)
+        {    
+            myAnimator.SetBool("moveFwd", true);
+
+            Vector3 direction = targetPosition - transform.position;
+            direction.y = 0; // Make sure the character doesn't tilt upwards.
+            if (direction != Vector3.zero)
+            {
+                Quaternion targetRotation = Quaternion.LookRotation(direction);
+                transform.rotation = Quaternion.Slerp(transform.rotation, targetRotation, Time.deltaTime * 10.0f); // Adjust the rotation speed here.
+            }
+
+            // Move the character towards the target position.
+            transform.position = Vector3.MoveTowards(transform.position, targetPosition, moveSpeed * Time.deltaTime);
+
+            // Check if the character has reached the target position.
+            if (Vector3.Distance(transform.position, targetPosition) < 0.1f)
+            {
+                isMoving = false;
+                myAnimator.SetBool("moveFwd", false);
+            }
+        }
+    }
+}
